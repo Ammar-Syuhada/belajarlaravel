01263b11781b7f009b001dc746e54d746a210f94
Ammar-Syuhada <mhdammarsyuhada@gmail.com>
2025-09-17 17:04:56 +0700

    Admin and User Controller

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,11 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Foundation\Auth\User as Authenticable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
     //tambahhkan ini
     use HasFactory;
