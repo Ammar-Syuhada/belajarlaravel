83f2cb72343c18f67aca1afa73839d0f5427576a
Ammar-Syuhada <mhdammarsyuhada@gmail.com>
2025-09-17 11:40:43 +0700

    Config Route

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,27 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('hello');
-});
-Route::get('/login',function(){
-    return view('akun.login');
-});
+// Guest Route
+Route::group(['middleware'=>'guest'],function(){
+    Route::get('/',function() {
+        return view('welcome');
+    });
+
+    Route::post('/post-login',[AuthController::class,'login']);
+})->middleware('guest');
+
+// admin Route
+Route::group(['middleware'=>'admin'],function(){
+    Route::get('/admin',[AdminControler::class,'dashboard'])->name('admin.dashboard');
+
+    Route::get('/admin-logout',[AuthController::class,'admin_logout'])->name('admin.logout');
+})->middleware('admin');
+
+// User Route
+Route::group(['middleware'=>'web'],function(){
+    Route::get('/user',[UserController::class,'index'])->name('user.logout');
+    
+    Route::get('/user-logout',[AuthController::class,'user_logout'])->name('user.logout');
+})->middleware('web');
