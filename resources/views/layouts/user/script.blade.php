478f79f77b229e56e9a0487a29ff9512141749f4
Ammar-Syuhada <mhdammarsyuhada@gmail.com>
2025-09-17 09:06:50 +0700

    Layout user

---
+++ resources/views/layouts/user/script.blade.php
@@ -0,0 +1,14 @@
+<script src="{{ asset('assets/templates/user/js/vendor/jquery-2.2.4.min.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
+    integrity="sha384-b/U6ypjBEHpOf/4t1nzFpr53nxSS+GLCkfwBdFNTTclqlqenlSfWAzpKaMNfNmj4"
+    crossorigin="anonymous"></script>
+<script src="{{ asset('assets/templates/user/js/vendor/bootstrap.min.js') }}"></script>
+<script src="{{ asset('assets/templates/user/js/jquery.ajaxchimp.min.js') }}"></script>
+<script src="{{ asset('assets/templates/user/js/jquery.nice-select.min.js') }}"></script>
+<script src="{{ asset('assets/templates/user/js/jquery.sticky.js') }}"></script>
+<script src="{{ asset('assets/templates/user/js/nouislider.min.js') }}"></script>
+<script src="{{ asset('assets/templates/user/js/jquery.magnific-popup.min.js') }}"></script>
+<script src="{{ asset('assets/templates/user/js/owl.carousel.min.js') }}"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6vrxywDD0VfAu6E"></script>
+<script src="{{ asset('assets/templates/user/js/gmaps.min.js') }}"></script>
+<script src="{{ asset('assets/templates/user/js/main.js') }}"></script>
